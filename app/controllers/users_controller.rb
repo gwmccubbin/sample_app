10d4300d64f7e7ff9937bccedb89ebeb9de64fb4
System Administrator <root@easymac.local>
2013-07-15 13:55:40 -0500

    Eliminate an unnecessary assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
   	if @user.save
       sign_in @user
   		flash[:success] = "Welcome to the Sample App!"
